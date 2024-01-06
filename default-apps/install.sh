5e57a79bc9db25fd300cabaef5bd6ab44a2dc8ae
guilherme-n-l <acc.guilhermenl@gmail.com>
2024-01-06 13:49:45 -0300

    Updating symlinks

--- default-apps/install.sh
+++ default-apps/install.sh
@@ -5,8 +5,7 @@ DEFAULT_LIST="$SCRIPT_DIR/default.list"
 MIMEAPPS_LIST="$HOME/.config/mimeapps.list"
 
 if [ -e "$DEFAULT_LIST" ]; then
-    cat "$DEFAULT_LIST" > "$MIMEAPPS_LIST"
-    echo "File copied successfully!"
+    ln -fs "$DEFAULT_LIST" "$MIMEAPPS_LIST"
 else
     echo "Error: default.list not found in the script directory."
     exit 1
